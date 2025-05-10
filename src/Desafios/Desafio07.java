ec3544cc1d53ca82b772dcebc819018974b23a74
LukReis <lucasesdras8@gmail.com>
2025-05-09 21:03:28 -0300

    Resolvendo Desafio do curso de switch

---
+++ src/Desafios/Desafio07.java
@@ -0,0 +1,35 @@
+package Desafios;
+
+import java.util.Scanner;
+
+public class Desafio07 {
+    public static void main (String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+
+        System.out.print("Valor do Empréstimo: ");
+        double valorEmprestimo = entrada.nextDouble();
+
+        System.out.print("Quantidade de Parcelas: ");
+        int quantidadeParcelas = entrada.nextInt();
+
+        double taxaJuros = switch (quantidadeParcelas) {
+            case 1, 2 -> valorEmprestimo * 0.0199;
+            case 3 -> valorEmprestimo * 0.0299;
+            default -> valorEmprestimo * 0.0399;
+
+        };
+
+        double tarifaFixa = valorEmprestimo <= 100 ? valorEmprestimo : valorEmprestimo + 1.50;
+
+        double totalJuros = valorEmprestimo * (taxaJuros / 100 * quantidadeParcelas);
+        double custoTotal = valorEmprestimo + tarifaFixa + totalJuros;
+
+
+        System.out.printf("Tarifa fixa: R$%.2f%n", tarifaFixa);
+        System.out.printf("Total de Juros: R$%.2f%n", totalJuros);
+        System.out.printf("Custo Total: R$%.2f%n", custoTotal);
+
+    }
+}
