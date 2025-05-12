1ed93b80d801f923db1075b1046804be059a4e0c
LukReis <lucasesdras8@gmail.com>
2025-05-11 21:51:00 -0300

    Aprendendo sobre estrutura de repetição

---
+++ src/Teoricas/ControlePesoAeronave.java
@@ -0,0 +1,16 @@
+package Teoricas;
+
+import java.util.Scanner;
+
+public class ControlePesoAeronave {
+    public static void main(String [] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Quantidade de passageiros: ");
+        int totalPassageiros = entrada.nextInt();
+
+
+        System.out.print("Quantidade do peso dos Passageiros");
+
+    }
+}
---
+++ src/Teoricas/estruturaRepeticaoFor.java
@@ -0,0 +1,9 @@
+package Teoricas;
+
+public class estruturaRepeticaoFor {
+    public static void main (String [] args) {
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
