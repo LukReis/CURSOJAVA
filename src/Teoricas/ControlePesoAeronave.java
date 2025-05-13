136dffaef7234db36e0060e05c02423b712466b8
LukReis <lucasesdras8@gmail.com>
2025-05-12 21:02:35 -0300

    Aprendendo sobre estrutura de repetição (FOR)

--- src/Teoricas/ControlePesoAeronave.java
+++ src/Teoricas/ControlePesoAeronave.java
@@ -6,11 +6,23 @@ public class ControlePesoAeronave {
     public static void main(String [] args) {
         Scanner entrada = new Scanner(System.in);
 
+        System.out.print("Peso Maximo da aeronave: ");
+        int pesoMaximo = entrada.nextInt();
+
         System.out.print("Quantidade de passageiros: ");
         int totalPassageiros = entrada.nextInt();
 
+        int pesoTotalPassageiros = 0;
 
-        System.out.print("Quantidade do peso dos Passageiros");
+        for (int passageiroAtual = 1; passageiroAtual <= totalPassageiros; passageiroAtual++) {
+            System.out.printf("Peso do passageiro #%d: ", passageiroAtual);
+            int pesoPassageiro = entrada.nextInt();
+
+            pesoTotalPassageiros += pesoPassageiro;
+        }
+        System.out.printf("Peso Máximo da aeronave: %d KG%n", pesoMaximo);
+        System.out.printf("Peso Total dos passageiros: %d KG%n", pesoTotalPassageiros);
+        System.out.printf("Situação da aeronave: %s%n", pesoTotalPassageiros > pesoMaximo ? "Peso excedido" : "Liberada");
 
     }
 }
