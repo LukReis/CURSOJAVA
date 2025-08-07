b470e0561c66c0cce0780ab325665ab33d663526
LukReis <lucasesdras8@gmail.com>
2025-08-07 20:51:18 -0300

    Estudos de Wrapper

---
+++ src/POO/aula06/Cliente.java
@@ -0,0 +1,8 @@
+package POO.aula06;
+
+public class Cliente {
+
+    String nome;
+    Integer idade;
+    Double rendaMensal;
+}
---
+++ src/POO/aula06/Principal1.java
@@ -0,0 +1,16 @@
+package POO.aula06;
+
+public class Principal1 {
+    public static void main(String[] args) {
+
+        // Tipos Primitivos
+        int diasParaEntrega;
+        long codigoEntrega;
+        float valorFrete;
+        double valorTotal;
+        char tipoCliente;
+        boolean compraPaga;
+
+        String nomeCliente;
+    }
+}
