49d67f812b3c891e0bcbc46aa046fc520debbe5c
LukReis <lucasesdras8@gmail.com>
2025-09-07 21:13:56 -0300

    Aprendendo Sobre gerenciamento de Memoria heap

---
+++ src/GerenciamentoDeMemoria/teste.java
@@ -0,0 +1,31 @@
+package GerenciamentoDeMemoria;
+
+public class teste {
+    public static void main(String[] args) {
+        imprimirUsoMemoria();
+
+        byte[] x =  new byte[500 * 1024 * 1024];
+        byte[] y =  new byte[500 * 1024 * 1024];
+
+        imprimirUsoMemoria();
+    }
+
+    static void imprimirUsoMemoria() {
+        System.out.printf("Maxima: %s%n",emMegaBytes(Runtime.getRuntime().maxMemory()));
+
+        System.out.printf("Total Empenhada: %s%n",emMegaBytes(Runtime.getRuntime().totalMemory()));
+
+        System.out.printf("Disponivel: %s%n",emMegaBytes(Runtime.getRuntime().freeMemory()));
+
+        long memoriaUsada = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+
+        System.out.printf("Usada: %s%n",emMegaBytes(memoriaUsada));
+
+        System.out.println("-----------------------------------");
+
+    }
+
+    static String emMegaBytes(long bytes) {
+        return String.format("%.2fMB", bytes / 1024d / 1024d );
+    }
+}
