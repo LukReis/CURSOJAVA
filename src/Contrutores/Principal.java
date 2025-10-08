68f786d047a3c2254d4f85855f8c9c2318fe0085
LukReis <lucasesdras8@gmail.com>
2025-10-08 20:36:35 -0300

    Aprendendo Sobre Construtores

---
+++ src/Contrutores/Principal.java
@@ -0,0 +1,16 @@
+package Contrutores;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Produto produto1 = new Produto("Picanha 1kg (peça)");
+
+
+
+        System.out.println(produto1.nome);
+        System.out.println(produto1.quantidadeEstoque);
+
+    }
+
+
+}
