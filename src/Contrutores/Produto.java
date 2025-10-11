c86fad2e18035da4c90c4f4139e0683012487beb
LukReis <lucasesdras8@gmail.com>
2025-10-10 22:10:36 -0300

    Aprendendo Sobre Construtores e Sobrecarga

--- src/Contrutores/Produto.java
+++ src/Contrutores/Produto.java
@@ -1,5 +1,7 @@
 package Contrutores;
 
+import java.util.Objects;
+
 public class Produto {
 
     static final int QUANTIDADE_ESTOQUE_INICIAL = 100;
@@ -7,12 +9,26 @@ public class Produto {
     String nome;
     int quantidadeEstoque;
 
+ Produto() {
+    this("sem nome");
+    }
+
  Produto(String nome) {
-     this.nome = nome;
-     this.quantidadeEstoque = QUANTIDADE_ESTOQUE_INICIAL;
+    this(nome, QUANTIDADE_ESTOQUE_INICIAL);
 
  }
 
+ Produto(String nome, int estoqueInicial) {
+     Objects.requireNonNull(nome, "Nome é Obrigatorio!");
+
+     if(estoqueInicial < 0) {
+         throw new IllegalArgumentException("Estoque inicial não pode ser negativo");
+     }
+
+
+     this.nome = nome;
+     this.quantidadeEstoque = estoqueInicial;
+ }
 
 
 
